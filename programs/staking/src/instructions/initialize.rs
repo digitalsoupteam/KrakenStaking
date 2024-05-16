155d25d7c27e62cde2c86f7314f1a03f00a04e51
inveker <alex.korolev.1995@gmail.com>
2024-05-16 20:39:54 +0300

    program.intialize :construction_worker:

--- programs/staking/src/instructions/initialize.rs
+++ programs/staking/src/instructions/initialize.rs
@@ -3,7 +3,13 @@ use anchor_lang::prelude::*;
 use crate::states::ProgramState;
 
 pub fn initialize(ctx: Context<Initialize>, args: InitializeArgs) -> Result<()> {
-   
+    let program = &mut ctx.accounts.state;
+    program.admin = args.admin;
+    msg!(
+        "Initialized program state: {:?} (size: {})",
+        program,
+        ProgramState::MAX_SIZE
+    );
     Ok(())
 }
 
@@ -14,6 +20,18 @@ pub struct InitializeArgs {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-   
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + ProgramState::MAX_SIZE, // TODO adjust this
+        seeds = [ProgramState::SEED],
+        bump
+    )]
+    pub state: Account<'info, ProgramState>,
+
+    pub system_program: Program<'info, System>,
 }
 
--- programs/staking/src/states.rs
+++ programs/staking/src/states.rs
@@ -2,3 +2,20 @@ use std::collections::HashSet;
 
 use anchor_lang::prelude::*;
 use crate::errors::ErrorCode;
+
+#[account]
+#[derive(Default, Debug)]
+pub struct ProgramState {
+    pub admin: Pubkey, // Admin's public key
+    pub new_admin: Option<Pubkey>,
+    pub last_vault_id: u32,
+}
+
+impl ProgramState {
+    pub const SEED: &'static [u8] = b"program_state";
+    pub const MAX_SIZE: usize = 32 // admin
+        + (1 + 32)  // new_admin
+        + 4 // last_vault_id
+        + 1024 // reserved
+    ;
+}
