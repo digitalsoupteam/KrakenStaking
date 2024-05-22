e71739e1df7b8bf10299cf2a0d153df4a7d11fe7
inveker <alex.korolev.1995@gmail.com>
2024-05-22 20:46:34 +0300

    program.unstake :construction_worker:

--- programs/staking/src/instructions/unstake.rs
+++ programs/staking/src/instructions/unstake.rs
@@ -7,6 +7,42 @@ use crate::events::Unstaked;
 use crate::states::{LockState, UserConfigState, VaultState};
 
 pub fn unstake(ctx: Context<Unstake>, _args: UnstakeArgs) -> Result<()> {
+    let user_lock = &mut ctx.accounts.user_lock;
+
+    if user_lock.locked_at + user_lock.locked_for > Clock::get()?.unix_timestamp as u32 {
+        return Err(ErrorCode::NotEligible.into());
+    }
+
+    // transfer back from vault_token -> user_token
+    anchor_spl::token::transfer_checked(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            anchor_spl::token::TransferChecked {
+                from: ctx.accounts.vault_token.to_account_info(),
+                to: ctx.accounts.user_token.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                authority: ctx.accounts.vault.to_account_info(),
+            },
+            &[&[
+                &VaultState::SEED,
+                ctx.accounts.vault.id.to_le_bytes().as_ref(),
+                &[ctx.bumps.vault]
+            ]]
+        ),
+        user_lock.amount,
+        ctx.accounts.mint.decimals,
+    )?;
+
+    emit!(Unstaked {
+        lock: user_lock.key(),
+        staker: ctx.accounts.staker.key(),
+        amount: user_lock.amount,
+        vault: ctx.accounts.vault.key(),
+        period: user_lock.locked_for,
+        id: user_lock.id,
+    });
+
+    user_lock.amount = 0;
     Ok(())
 }
 
@@ -20,6 +56,58 @@ pub struct UnstakeArgs {
 #[derive(Accounts)]
 #[instruction(vault_id: u32, stake_id: u32)]
 pub struct Unstake<'info> {
-    
+    #[account(mut)]
+    pub staker: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [LockState::SEED, staker.key().as_ref(), stake_id.to_le_bytes().as_ref()],
+        constraint = user_lock.staker == staker.key() @ ErrorCode::InvalidOwner,
+        constraint = user_lock.amount > 0 @ ErrorCode::AlreadyUnstaked,
+        has_one = vault,
+        has_one = staker,
+        bump
+    )]
+    pub user_lock: Account<'info, LockState>,
+
+    #[account(
+        mut,
+        seeds = [VaultState::SEED, vault_id.to_le_bytes().as_ref()],
+        constraint = vault_id == vault.id @ ErrorCode::InvalidVaultId, 
+        constraint = vault.paused == false @ ErrorCode::VaultIsPaused,
+        has_one = mint,
+        bump
+    )]
+    pub vault: Account<'info, VaultState>,
+
+    #[account(
+        seeds = [UserConfigState::SEED, staker.key().as_ref()],
+        constraint = user_config.magic == UserConfigState::MAGIC,
+        bump
+    )]
+    pub user_config: Account<'info, UserConfigState>,
+
+    #[account(
+        mut,
+        constraint = user_token.owner == staker.key() @ ErrorCode::InvalidOwner,
+        has_one = mint @ ErrorCode::InvalidVaultMint,
+    )]
+    pub user_token: Account<'info, TokenAccount>,
+
+    #[account(mint::token_program = Token::id())]
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        constraint = vault_token.owner == vault.key(),
+        has_one = mint
+    )]
+    pub vault_token: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub system_program: Program<'info, System>,
 }
 
